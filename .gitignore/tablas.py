2ea46819f0c427a5f4aa945a79c5570784a497a6
ivanh96 <ivanh96@hotmail.com>
2018-03-09 09:31:57 -0600

    Update tablas.py

--- .gitignore/tablas.py
+++ .gitignore/tablas.py
@@ -1,7 +1,6 @@
 %%sql 
 
 
-# TABLA EMPLEADOS
 CREATE TABLE empleados (
     empleadoID INT NOT NULL,
     nombre VARCHAR(50),
@@ -16,11 +15,10 @@ CREATE TABLE empleados (
     puesto VARCHAR(50),
     tipo_empleado VARCHAR(50),
     salario FLOAT(50),
-    PRIMARY KEY (empleadoID),
+    PRIMARY KEY (empleadoID)
 );
 
 
-# VEHICULOS
 CREATE TABLE vehiculos (
     vehiculoID INT NOT NULL,
     placa VARCHAR(50),
@@ -28,64 +26,58 @@ CREATE TABLE vehiculos (
     marca VARCHAR(50),
     año VARCHAR(50),
     color VARCHAR(50),
-    PRIMARY KEY (vehiculoID),
+    PRIMARY KEY (vehiculoID)
 );
 
 
-# VEHICULOS ASIGNADOS
 CREATE TABLE vehiculos_asignados (
     vehiculoID INT,
     empleadoID INT,
     dia_semana VARCHAR(50),
     FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID),
-    FOREIGN KEY (vehiculoID) REFERENCES vehiculos(vehiculoID),
+    FOREIGN KEY (vehiculoID) REFERENCES vehiculos(vehiculoID)
 );
 
 
-# HISTORIAL
 CREATE TABLE historial (
     empleadoID INT,
     puestoID INT NOT NULL,
     fecha_puesto DATE,
     salario FLOAT(50),
     PRIMARY KEY (puestoID),
-    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID),
+    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID)
 );
 
 
-# CAPACITACIONES
 CREATE TABLE capacitaciones (
     empleadoID INT,
     fecha_capacitacion DATE,
     capacitacion VARCHAR(50),
     calificacion FLOAT(50),
-    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID),
+    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID)
 );
 
 
-# ACCIONES DISCIPLINARIAS
 CREATE TABLE acciones (
     empleadoID INT,
     fecha_accion DATE,
     accion_otorgada VARCHAR(50),
     explicacion VARCHAR(100),
-    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID),
+    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID)
 );
 
 
-# ADMINISTRATIVOS
 CREATE TABLE administrativos (
     empleadoID INT,
     oficina VARCHAR(50),
     tel_oficina VARCHAR(50),
-    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID),
+    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID)
 );
 
 
-# OPERATIVOS
 CREATE TABLE operativos (
     empleadoID INT,
     tipo_licencia VARCHAR(10),
     uso_lentes VARCHAR(50),
-    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID),
+    FOREIGN KEY (empleadoID) REFERENCES epleados(empleadoID)
 );
